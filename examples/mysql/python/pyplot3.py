4a8dd576f48224f46bfbbd908a494b1a5794989e
Krister Trangius <krister@trangius.se>
2025-09-18 10:11:24 +0200

    asdf

--- examples/mysql/python/pyplot3.py
+++ examples/mysql/python/pyplot3.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-x = [5, 7, 8, 7, 6, 9, 5, 6, 7, 8]
+x = [5,  7,  8,  7,  6,   9,  5,   6,  7,  8]
 y = [99, 86, 87, 88, 100, 86, 103, 87, 94, 78]
 
 plt.scatter(x, y)             # punktdiagram
